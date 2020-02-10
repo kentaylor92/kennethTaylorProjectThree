74252e1129a6cdfd5dcdc59c42faffdd49a6b75f
kentaylor92 <ken_seaway@gmail.com>
2020-02-10 17:33:03 -0500

    Revamped HTML/CSS, added Jquery logic

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
 
-$(document).ready(function() {    
-    const cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+$(document).ready(function() {
 
-    for (i = 0; i < cards.length; i++) {
-        const shuffleCards = Math.floor(Math.random() * cards.length);
-        console.log(shuffleCards);  
+
+
+// Target memoryCard Div
+const cards = $('.memoryCard');
+// const frontCard = $('.front');
+
+// for statement to randomize card order:
+// for (let i = 0; i < cards.length; i++) {
+//     let shuffledCards = Math.floor(Math.random() * cards.length);
+//     cards = shuffledCards;
+// }
+
+
+
+
+let flipped = false;
+let firstCard;
+let secondCard;
+
+
+// Toggle "flip" class
+function flipCard() {
+    $(cards).on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('flip');
+        // first card selection
+        if (flipped === false) {
+            flipped = true;
+            firstCard = this;
+        //  2nd card selection
+        } else {
+            flipped = false;
+            secondCard = this;
+                      
+            checkForMatch();
+        }
+    })
+    
+}
+flipCard();
+
+function checkForMatch() {
+    if (firstCard.dataset.icon === secondCard.dataset.icon) {
+        $(firstCard).unbind('click');
+        $(secondCard).unbind('click');
+
+    } else {
+        setTimeout(() => {
+            $(firstCard).toggleClass('flip');
+            $(secondCard).toggleClass('flip');
+            console.log("Not a match");
+        }, 1000);
     }
+}
 
-});
+});  //end doc ready
+
+
+
 
